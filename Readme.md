a242a12772ac9bb84b95b81399436373b0863dc3
M Woods <mkowoods@gmail.com>
2014-11-25 10:58:15 -0800

    First Commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+A simple flask based app to allow friends to post interesting articles/blog posts to a common site to create a private feed.
+
+It's like a chat room for a small group of friends.
---
+++ static/script.js
@@ -0,0 +1,27 @@
+var main = function() {
+  $('.article').click(function() {
+    $('.article').removeClass('current');
+    $('.description').hide();
+
+    $(this).addClass('current');
+    $(this).children('.description').show();
+  });
+
+  $(document).keypress(function(event) {
+    if(event.which === 111) {
+      $('.description').hide();
+
+      $('.current').children('.description').show();
+    }
+
+    else if(event.which === 110) {
+      var currentArticle = $('.current');
+      var nextArticle = currentArticle.next();
+      
+      currentArticle.removeClass('current');
+      nextArticle.addClass('current');
+    }
+  });
+}
+
+$(document).ready(main);
