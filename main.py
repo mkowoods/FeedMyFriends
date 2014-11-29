182b248b1e6b5e293631b4dc87cf3add14ed10b4
M Woods <mkowoods@gmail.com>
2014-11-29 12:51:01 -0800

    Minor Bug Fixes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
@@ -9,4 +11,7 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug= True)
+    port = int(os.environ.get("PORT", 5000))
+    if port == 5000:
+        app.debug = True
+    app.run(host='0.0.0.0', port=port)
